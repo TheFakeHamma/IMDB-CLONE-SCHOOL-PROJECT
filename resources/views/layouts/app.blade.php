81ae7a1db5871c857269ea88500178eaf9e69116
thefakehamma <hamma.khoshnaw@gmail.com>
2024-05-04 06:52:12 +0200

    Change from bootstrap to tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
