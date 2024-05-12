17c26a8390af8749ccd066ec7766d953123c8c6f
thefakehamma <hamma.khoshnaw@gmail.com>
2024-05-12 03:07:14 +0200

    Change width

--- resources/views/contents.blade.php
+++ resources/views/contents.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-[100rem] mx-auto px-4 sm:px-6 lg:px-8">
         @php
             $heading = 'All Content'; // Default heading
 
